09a479fad018cc1b776c50d77719aab9e24dca46
kaiware <kaiware4job@gmail.com>
2026-03-24 23:11:52 +0900

    16 ファイルシステム 完了

--- common.h
+++ common.h
@@ -24,6 +24,8 @@ typedef uint32_t vaddr_t; // 仮想メモリアドレスを表す == uintptr_t
 #define SYS_PUTCHAR 1
 #define SYS_GETCHAR 2
 #define SYS_EXIT 3
+#define SYS_READFILE 4
+#define SYS_WRITEFILE 5
 #define SCAUSE_ECALL 8
 
 void *memcpy(void *dst, const void *src, size_t n);
--- user.h
+++ user.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "common.h"
 
+int readfile(const char *filename, char *buf, int len);
+int writefile(const char *filename, const char *buf, int len);
+
 __attribute__((noreturn)) void exit(void);
 
 void putchar(char ch);
-
 int getchar(void);
